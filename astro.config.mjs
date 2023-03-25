975eaa5ae9fc910aba0f3084c6d9574e4c053200
Joshua Byrd <phocks@gmail.com>
2023-03-25 17:08:05 +1000

    Move to a newer install

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
-import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/edge";
-import react from "@astrojs/react";
-import svelte from "@astrojs/svelte";
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: vercel({
-    analytics: false,
-  }),
-  integrations: [svelte(), react()],
+	site: 'https://example.com',
+	integrations: [mdx(), sitemap()],
 });
