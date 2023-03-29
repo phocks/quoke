d6131f9f838ea5db3905c98ded1f6fca3e3ae2bb
Joshua Byrd <phocks@gmail.com>
2023-03-29 20:07:28 +1000

    No Prisma?

--- src/pages/script.ts
+++ src/pages/script.ts
@@ -1,74 +1,76 @@
-import { PrismaClient } from "@prisma/client";
+// import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
-async function main() {
-  // const user = await prisma.user.create({
-  //   data: {
-  //     name: "Alice",
-  //     email: "alice@prisma.io",
-  //   },
-  // });
+// async function main() {
+//   // const user = await prisma.user.create({
+//   //   data: {
+//   //     name: "Alice",
+//   //     email: "alice@prisma.io",
+//   //   },
+//   // });
 
-  // console.log(user);
+//   // console.log(user);
 
-  // const users = await prisma.user.findMany()
-  // console.log(users)
+//   // const users = await prisma.user.findMany()
+//   // console.log(users)
 
-  // await prisma.user.create({
-  //   data: {
-  //     name: "Alice",
-  //     email: "alice@prisma.io",
-  //     posts: {
-  //       create: { title: "Hello World" },
-  //     },
-  //     profile: {
-  //       create: { bio: "I like turtles" },
-  //     },
-  //   },
-  // });
+//   // await prisma.user.create({
+//   //   data: {
+//   //     name: "Alice",
+//   //     email: "alice@prisma.io",
+//   //     posts: {
+//   //       create: { title: "Hello World" },
+//   //     },
+//   //     profile: {
+//   //       create: { bio: "I like turtles" },
+//   //     },
+//   //   },
+//   // });
 
-  const allUsers = await prisma.user.findMany({
-    include: {
-      posts: true,
-      profile: true,
-    },
-  });
+//   const allUsers = await prisma.user.findMany({
+//     include: {
+//       posts: true,
+//       profile: true,
+//     },
+//   });
 
-  console.dir(allUsers, { depth: null });
-}
+//   console.dir(allUsers, { depth: null });
+// }
 
-import type { APIRoute } from "astro";
+// import type { APIRoute } from "astro";
 
-export const get: APIRoute = async ({ params }) => {
-  const id = params.id;
-  console.log(params);
+// export const get: APIRoute = async ({ params }) => {
+//   const id = params.id;
+//   console.log(params);
 
-  const json = { message: "hello" };
+//   const json = { message: "hello" };
 
-  // main()
-  //   .then(async () => {
-  //     await prisma.$disconnect();
-  //   })
-  //   .catch(async (e) => {
-  //     console.error(e);
-  //     await prisma.$disconnect();
-  //     // process.exit(1);
-  //   });
+//   // main()
+//   //   .then(async () => {
+//   //     await prisma.$disconnect();
+//   //   })
+//   //   .catch(async (e) => {
+//   //     console.error(e);
+//   //     await prisma.$disconnect();
+//   //     // process.exit(1);
+//   //   });
 
-  const allUsers = await prisma.user.findMany({
-    include: {
-      posts: true,
-      profile: true,
-    },
-  });
+//   const allUsers = await prisma.user.findMany({
+//     include: {
+//       posts: true,
+//       profile: true,
+//     },
+//   });
 
-  console.dir(allUsers, { depth: null });
+//   console.dir(allUsers, { depth: null });
 
-  return new Response(JSON.stringify(allUsers), {
-    status: 200,
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-};
+//   return new Response(JSON.stringify(allUsers), {
+//     status: 200,
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//   });
+// };
+
+export {}
