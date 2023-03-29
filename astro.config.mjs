cf024034389e6a8a310f231a3413214e57d16875
Joshua Byrd <phocks@gmail.com>
2023-03-29 19:58:42 +1000

    Edge?

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-import vercel from "@astrojs/vercel/serverless";
+// import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/edge';
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel(),
-  site: "https://example.com",
+  adapter: vercel({
+    analytics: true
+  }),
+  site: "https://quoke.co",
   integrations: [mdx()]
 });
