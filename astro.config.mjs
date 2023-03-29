40a7c9f874ac53d9f4422a708ac0ed1b07c79143
Joshua Byrd <phocks@gmail.com>
2023-03-30 08:42:33 +1000

    Can I edge now?

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-import vercel from "@astrojs/vercel/serverless";
+// import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/edge";
+
 
 // https://astro.build/config
 export default defineConfig({
