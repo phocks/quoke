f16b24a4a22b428a7e134f964b6831fa36366ac3
Joshua Byrd <phocks@gmail.com>
2023-03-30 09:00:45 +1000

    Back to Node serverless

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-// import vercel from "@astrojs/vercel/serverless";
-import vercel from "@astrojs/vercel/edge";
+import vercel from "@astrojs/vercel/serverless";
+// import vercel from "@astrojs/vercel/edge";
 
 
 // https://astro.build/config
